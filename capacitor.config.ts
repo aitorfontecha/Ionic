c471a1ed01fc3447dedd67ceba057af1606fba94
fontechaaitor <fontechaaitor@gmail.com>
2022-05-04 17:02:28 +0200

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'sgta_scraping',
+  webDir: 'build',
+  bundledWebRuntime: false
+};
+
+export default config;
